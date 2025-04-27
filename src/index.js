e1c92b6999cf116b4964a3c9d7866fcd9cbce75e
Leonel Donati <leonelroman321@gmail.com>
2025-04-27 15:52:24 -0300

    added morgan

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { port } from "./config.js";
 import { productRoutes } from "./routes/products.js";
+import morgan from "morgan";
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(productRoutes);
 app.listen(port, () => {
